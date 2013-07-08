6f22a9320d27dc716590c520716bcfe53facca3f
Rodolfo Neugebauer <rodolfo.neugebauer@gmail.com>
2013-07-08 14:55:22 -0300

    commit inicial

---
+++ artifact_id_testeGit/src/main/java/group_id_teste/artifact_id_testeGit/App.java
@@ -0,0 +1,13 @@
+package group_id_teste.artifact_id_testeGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! with git" );
+    }
+}
